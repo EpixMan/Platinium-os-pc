2628c20267bdbd3d91a79ce671b9345ff4f3b78c
EpixMan <65122296+EpixMan@users.noreply.github.com>
2026-02-18 21:59:28 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ set -ouex pipefail
 #Niri stuff:
 #sudo dnf copr enable avengemedia/dms -y
 #sudo dnf install niri dms -y
-rpm-ostree install akmod-nvidia libreoffice podman-compose fastfetch helix git-credential-libsecret polkit xdg-user-dirs dbus-tools dbus-daemon gnome-keyring pavucontrol google-noto-emoji-fonts gnome-disk-utility gparted wireplumber pipewire pamixer network-manager-applet NetworkManager-openvpn NetworkManager-openconnect pipewire-alsa wlr-randr wlsunset brightnessctl foot fish fontawesome-fonts-all gnome-themes-extra gnome-icon-theme paper-icon-theme breeze-icon-theme papirus-icon-theme kvantum adw-gtk3-theme gstreamer1-vaapi gstreamer1-plugins-bad-free-extras gstreamer1-plugin-libav -y
+rpm-ostree install libreoffice podman-compose fastfetch helix git-credential-libsecret polkit xdg-user-dirs dbus-tools dbus-daemon gnome-keyring pavucontrol google-noto-emoji-fonts gnome-disk-utility gparted wireplumber pipewire pamixer network-manager-applet NetworkManager-openvpn NetworkManager-openconnect pipewire-alsa wlr-randr wlsunset brightnessctl foot fish fontawesome-fonts-all gnome-themes-extra gnome-icon-theme paper-icon-theme breeze-icon-theme papirus-icon-theme kvantum adw-gtk3-theme gstreamer1-vaapi gstreamer1-plugins-bad-free-extras gstreamer1-plugin-libav -y
 #todo: install real ffmpeg
 
 #hyprland stuff
