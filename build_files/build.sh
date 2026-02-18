aff57bbd6c92e4623d9e0548f01a54e59e0a03e7
EpixMan <65122296+EpixMan@users.noreply.github.com>
2026-02-18 06:11:17 +0300

    Add RPM Fusion repository installation to build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -ouex pipefail
-
+sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 #To install COSMIC
 #dnf5 copr enable -y ryanabx/cosmic-epoch
 #dnf5 -y install @cosmic-desktop @cosmic-desktop-apps
