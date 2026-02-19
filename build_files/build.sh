997d8d7cb6724b850f524df7a4d4e3cee34e4bba
EpixMan <65122296+EpixMan@users.noreply.github.com>
2026-02-19 13:17:55 +0300

    Update Nix installation command in build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,8 +20,7 @@ rpm-ostree install libreoffice podman-compose fastfetch helix git-credential-lib
 #hyprland stuff
 #dnf copr enable solopasha/hyprland -y
 #dnf install hyprland-plugins hyprlock hypridle hyprpaper hyprshot xdg-desktop-portal-hyprland hyprland -y
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install -- --no-start-daemon -- --no-confirm 
-
+curl -fsSL https://install.determinate.systems/nix | sh -s -- install linux --init none --no-confirm
 #dnf copr enable avengemedia/dms -y
 #dnf install dms -y
 #Removed bpytop
