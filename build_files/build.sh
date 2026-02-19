007f3b14468b87fb5789dd33342b00254dd3f2b7
EpixMan <65122296+EpixMan@users.noreply.github.com>
2026-02-19 06:30:04 +0300

    Modify nix installation command in build.sh
    Added --no-start-daemon option to nix installation.

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ rpm-ostree install libreoffice podman-compose fastfetch helix git-credential-lib
 #hyprland stuff
 #dnf copr enable solopasha/hyprland -y
 #dnf install hyprland-plugins hyprlock hypridle hyprpaper hyprshot xdg-desktop-portal-hyprland hyprland -y
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm
+curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm --no-start-daemon
 
 #dnf copr enable avengemedia/dms -y
 #dnf install dms -y
