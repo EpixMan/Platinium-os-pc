9542d8a2145199685607d8b7eaeff70325dd5cc3
EpixMan <65122296+EpixMan@users.noreply.github.com>
2026-02-19 04:41:13 +0300

    Comment out Hyprland installation in build.sh
    Comment out Hyprland installation commands in build script.

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,8 +18,8 @@ rpm-ostree install libreoffice podman-compose fastfetch helix git-credential-lib
 #todo: install real ffmpeg
 
 #hyprland stuff
-dnf copr enable solopasha/hyprland -y
-dnf install hyprland-plugins hyprlock hypridle hyprpaper hyprshot xdg-desktop-portal-hyprland hyprland -y
+#dnf copr enable solopasha/hyprland -y
+#dnf install hyprland-plugins hyprlock hypridle hyprpaper hyprshot xdg-desktop-portal-hyprland hyprland -y
 
 dnf copr enable avengemedia/dms -y
 dnf install dms -y
